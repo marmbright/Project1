6dbb12da536c4ffffc54b182b438652e86c4cf5c
marmbright <marmbright@verizon.net>
2020-02-14 13:36:00 -0500

    suicide

--- src/Warehouse.java
+++ src/Warehouse.java
@@ -18,13 +18,26 @@ public class Warehouse {
                 totPrice = totPrice + temp.price;
             }
         }
+        return totPrice;
     }
 
-    public int getTotInv(){
-        return totInv;
+    public int getTotInv(ArrayList<BikePart> list){
+        int quant = 0;
+        for (int i = 0; i < list.size(); i++) {
+            BikePart temp = list.get(i);
+            quant = quant + temp.quantity;
+        }
+        return quant;
     }
 
-    public int getInvOnSale() {
-        return invOnSale;
+    public int getInvOnSale(ArrayList<BikePart> list) {
+        int quantSale = 0;
+        for (int i = 0; i < list.size(); i++) {
+            BikePart temp = list.get(i);
+            if (temp.onSale) {
+                quantSale++;
+            }
+        }
+        return quantSale;
     }
 }
