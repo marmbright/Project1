8ea4a9ade321507192802bf078cfd6d8720b9b57
marmbright <marmbright@verizon.net>
2020-02-14 12:36:59 -0500

    Warehouse (Megan)

--- src/Warehouse.java
+++ src/Warehouse.java
@@ -3,5 +3,7 @@ public class Warehouse {
     public String name;
     public int totInv;
     public int invOnSale;
-    public double price; 
+    public double price;
+
+    public Warehouse( )
 }
