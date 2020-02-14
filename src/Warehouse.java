84a39c5d1ae267115ac2062d7379ef4135a22324
marmbright <marmbright@verizon.net>
2020-02-14 13:28:19 -0500

    murder

--- src/Warehouse.java
+++ src/Warehouse.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Warehouse {
 
     public String name;
@@ -5,9 +7,24 @@ public class Warehouse {
     public int invOnSale;
     public double price;
 
-    public double getPrice(){
+    public double getPrice(ArrayList<BikePart> list) {
+        double totPrice = 0.0;
 
+        for (int i = 0; i < list.size(); i++) {
+            BikePart temp = list.get(i);
+            if (temp.onSale) {
+                totPrice = totPrice + temp.salesPrice;
+            } else {
+                totPrice = totPrice + temp.price;
+            }
+        }
     }
 
+    public int getTotInv(){
+        return totInv;
+    }
 
+    public int getInvOnSale() {
+        return invOnSale;
+    }
 }
