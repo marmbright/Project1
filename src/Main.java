1d041783cf494cb73e9761b783ae3fc1637471fd
marmbright <marmbright@verizon.net>
2020-02-15 16:18:21 -0500

    Updated BikeParts.txt to contain quantity

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Scanner input= new Scanner( System.in );
 
-
         System.out.println("Enter Input Filename: ");
         String readfile = input.next();
 
