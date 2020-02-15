ab99dad1080e5afffecb88214894d7d75ddb796e
cjesus1701 <cjesus@mail.umw.edu>
2020-02-15 14:00:59 -0500

    Chris made this Test class to test how to have an interface with the code in main.

---
+++ src/Test.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner in = new Scanner( System.in );
+        boolean quitFlag = false;
+
+        while (quitFlag == false){
+            System.out.println("Select your option: \nReadFile\n" +
+                                "Enter BikePart\nSell BikePart\n" +
+                                "Display BikePart\nSortBy Name\n" +
+                                "SortBy Number\nQuit");
+            String user_input = in.nextLine();
+            if (user_input.equals("Quit")) {
+                quitFlag = true;
+            } else if (user_input.equals("Help")) {
+
+            }
+
+
+        }
+    }
+}
