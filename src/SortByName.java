f2d80b6847ca373fcceaf2d7309d88f5ac46e115
cjesus1701 <cjesus@mail.umw.edu>
2020-02-19 11:21:31 -0500

    Christopher's updated class files

--- src/SortByName.java
+++
@@ -1,6 +0,0 @@
-import java.util.Comparator;
-public class SortByName implements Comparator<BikePart> {
-    public int compare(BikePart x, BikePart y) {
-        return x.getPartName().compareToIgnoreCase(y.getPartName());
-    }
-}
