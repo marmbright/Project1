de5350fe192769311df2d8a51cdbcfc358791674
agronx <alexander.grondin@gmail.com>
2020-03-17 22:20:19 -0400

    Sortbys now can sort through vans or warehouse

--- src/Test.java
+++ src/Test.java
@@ -227,12 +227,42 @@ public class Test {
 
                     //Sort By Name
                 } else if (user_input.equalsIgnoreCase("Sort By Name")) {
-                    sortName(list);
+                    System.out.println("Which list would you like to sort?");
+                    String whichList = in.nextLine();
+                    if (whichList.equalsIgnoreCase( "van" )){
+                        System.out.println("Which Van?");
+                        String van= in.nextLine();
+                        for (Van item: fleet) {
+                            if (van.equalsIgnoreCase( item.vanName )){
+                                sortName( item.vanInv );
+                            } else {
+                                System.out.println("This van doesn't exist.");
+                            }
+
+                        }
+                    }else  {
+                        sortName( list );
+                    }
+
 
                     //Sort By Number
                 } else if (user_input.equalsIgnoreCase("Sort By Number")) {
-                    sortNum(list);
+                    System.out.println("Which list would you like to sort?");
+                    String whichList = in.nextLine();
+                    if (whichList.equalsIgnoreCase( "van" )){
+                        System.out.println("Which Van?");
+                        String van= in.nextLine();
+                        for (Van item: fleet) {
+                            if (van.equalsIgnoreCase( item.vanName )){
+                                sortNum( item.vanInv );
+                            } else {
+                                System.out.println("This van doesn't exist.");
+                            }
 
+                        }
+                    }else  {
+                        sortNum( list );
+                    }
                     //Add Van To Fleet
                 } else if (user_input.equalsIgnoreCase("Add Van To Fleet")) {
                     System.out.println("Enter van name: ");
