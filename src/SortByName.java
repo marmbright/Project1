fb0a7d4f5aede59801d44dc9c45b3cfa0c422ab7
marmbright <marmbright@verizon.net>
2020-03-18 15:48:48 -0400

    java doc for all classes except test

--- src/SortByName.java
+++ src/SortByName.java
@@ -1,5 +1,13 @@
 import java.util.Comparator;
+
 public class SortByName implements Comparator<BikePart> {
+
+    /**
+     * Compares two bikeparts to organize alphabetically by part name
+     * @param x a bikepart
+     * @param y a bikepart
+     * @return returns the parts in order
+     */
     public int compare(BikePart x, BikePart y) {
         return x.getPartName().compareToIgnoreCase(y.getPartName());
     }
--- src/Van.java
+++ src/Van.java
@@ -1,22 +1,42 @@
 import java.util.ArrayList;
 
+/**
+ * Class used to create van objects
+ */
 public class Van {
     public String vanName;
     public ArrayList<BikePart> vanInv;
 
+    /**
+     * Van constructor
+     * @param name the name of the van
+     * @param vanInv the vans inventory
+     */
     public Van(String name, ArrayList<BikePart> vanInv){
         this.vanName = name;
         this.vanInv = vanInv;
     }
 
+    /**
+     * getter for the name of the van
+     * @return returns the name of the van
+     */
     public String getVanName() {
         return vanName;
     }
 
+    /**
+     * getter for the van inventory
+     * @return returns the van inventory
+     */
     public ArrayList<BikePart> getVanInv() {
         return vanInv;
     }
 
+    /**
+     * setter for the van inventory
+     * @param vanInv arraylist of the van inventory
+     */
     public void setVanInv(ArrayList<BikePart> vanInv) {
         this.vanInv = vanInv;
     }
